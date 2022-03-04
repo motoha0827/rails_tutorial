85f7d61e53368bb4cb71ded1974452b8f35da9f4
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-03-04 10:44:47 +0900

    a

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-    def hello
-        render html: "¡Hola, world!"
-    end
-
-    def goodbye
-        render html: "goodbye, world!testtest"
-    end
-end
