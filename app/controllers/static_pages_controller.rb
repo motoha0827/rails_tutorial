b4f2cdb02ec020ddbb7d3abc5d435fe9329fd5c1
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-03-07 15:50:52 +0900

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
-  def ...
+  def home
+  end
+  
+  def help
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
--- test/controllers/chapter3_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Chapter3ControllerTest < ActionDispatch::IntegrationTest
-  test "should get ..." do
-    get chapter3_..._url
-    assert_response :success
-  end
-
-end
