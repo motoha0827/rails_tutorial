268b92615b2aadfa366bdeb6b076df2b5cc6f058
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-03-22 15:58:40 +0900

    10.45 add pagination

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2022_03_22_012709) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
