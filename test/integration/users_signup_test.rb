99d8d49e9098c9799880a2b22016ad9e7b102f2a
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-03-25 05:29:40 +0900

    fix: blank line&indent

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,10 +5,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+      post users_path, params: { 
+        user: { name:                  "",
+                email:                 "user@invalid",
+                password:              "foo",
+                password_confirmation: "bar" } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
