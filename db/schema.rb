3abbbf5d2b598cb5c60f5b40f4c28b81e693e010
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-03-29 11:04:40 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_25_044245) do
+ActiveRecord::Schema.define(version: 2022_03_29_013747) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", null: false
@@ -26,4 +36,6 @@ ActiveRecord::Schema.define(version: 2022_03_25_044245) do
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "microposts", "users"
 end
