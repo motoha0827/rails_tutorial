c086387cbb8cab086ba70b72d3d4c4b8ceda78a1
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-03-31 14:45:05 +0900

    14.13 add: followers test

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:   "Relationship",
+                                   foreign_key:  "followed_id",
+                                   dependent:    :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
