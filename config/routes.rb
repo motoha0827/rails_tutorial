bd30e2344cd492a8d8313b46762522daa9411321
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-03-31 14:59:08 +0900

    14.16 add: stats partial

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,12 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
-  resources :account_activations, only: %s(edit)
-  resources :password_resets,     only: %i(new create edit update)
-  resources :microposts,          only: %i(create destroy)
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 end
