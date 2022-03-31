101105c80eaaf1c97bcb6c3507d1ed368bc34752
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-03-31 15:09:38 +0900

    14.20 add: routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
+  resources :account_activations, only: %i[edit]
+  resources :password_resets,     only: %i[new create edit update]
+  resources :microposts,          only: %i[create destroy]
+  resources :relationships,       only: %i[create destroy]
+
 end
