1ac5fe9ed3eae46a57ccb30c7b60d3b06b6c8d69
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-03-31 10:01:27 +0900

    fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,9 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
-  
+
   def help
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
+  resources :account_activations, only: %s(edit)
+  resources :password_resets,     only: %i(new create edit update)
+  resources :microposts,          only: %i(create destroy)
 end
